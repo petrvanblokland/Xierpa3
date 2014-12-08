1aa509a2219a8ca93ea8f7b7113a5fc101420df3
petrvanblokland <buro@petr.com>
2014-12-08 22:09:48 +0100

    Got shadows to work. Default can be seen in PosterHead.

--- xierpa3/attributes/shadow.py
+++ xierpa3/attributes/shadow.py
@@ -16,43 +16,42 @@ from xierpa3.attributes.values import asValue
 
 class Shadow(Attribute):
     
-    def __init__(self, t=None, r=None, b=None, l=None, color=None, **kwargs):
+    def __init__(self, x=0, y=0, blur=None, color=None, **kwargs):
         #
         # The Shadow class builds the prefix dependent CSS syntax for a shadow attribute.
         #
         #    boxshadow=Shadow(0, 4, 10, -2, '#333'),
         #
-        #    box-shadow: 0 4px 10px -2px #333;
-        #    -webkit-box-shadow: 0 4px 10px -2px #333;
-        #    -ms-box-shadow: 0 4px 10px -2px #333;
-        #    -moz-box-shadow: 0 4px 10px -2px #333;
-        #    -o-box-shadow: 0 4px 10px -2px #333;
+        #    box-shadow: 0 4px 10px #333;
+        #    -webkit-box-shadow: 0 4px 10px #333;
+        #    -ms-box-shadow: 0 4px 10px #333;
+        #    -moz-box-shadow: 0 4px 10px #333;
+        #    -o-box-shadow: 0 4px 10px #333;
         #
         #    With different prefix values
-        #    boxshadow=Shadow(0, 4, 10, -2, '#333', webkit=Shadow(0, 8, 10, -2, '#FFF')),
+        #    boxshadow=Shadow(0, 4, 10, '#333', webkit=Shadow(0, 8, 10, '#FFF')),
         #
-        #    box-shadow: 0 4px 10px -2px #333;
-        #    -webkit-box-shadow: 0 8px 10px -2px #FFF;
-        #    -ms-box-shadow: 0 4px 10px -2px #333;
-        #    -moz-box-shadow: 0 4px 10px -2px #333;
-        #    -o-box-shadow: 0 4px 10px -2px #333;
+        #    box-shadow: 0 4px 10px #333;
+        #    -webkit-box-shadow: 0 8px 10px #FFF;
+        #    -ms-box-shadow: 0 4px 10px #333;
+        #    -moz-box-shadow: 0 4px 10px #333;
+        #    -o-box-shadow: 0 4px 10px #333;
         #
-        self.t = t or 0
-        self.r = r or 4
-        self.b = b or 10
-        self.l = l or -2
+        self.x = x or 0
+        self.y = y or 4
+        self.blur = blur or 10
         self.color = color or Color('#333')
         self.initializePrefixes(kwargs) # Initialize any child prefix attributes
     
     # self.value
        
     def _get_value(self):
-        return '%s %s %s %s %s' % (asValue(self.t), asValue(self.r), asValue(self.b), asValue(self.l), self.color)
+        return '%s %s %s %s' % (asValue(self.x), asValue(self.y), asValue(self.blur), self.color)
         
     value = property(_get_value)        
 
     def _get_raw(self):
-        return self.id, self.t, self.r, self.b, self.l, self.color
+        return self.id, self.x, self.y, self.blur, self.color
     
     raw = property(_get_raw)
 
@@ -60,7 +59,7 @@ class Shadow(Attribute):
         # Build the instance output on the (sass/css) builder
         if prefix is None: # This is the top call, do the other prefixes
             for prefix in self.C.PREFIXES:
-                shadow = self.prefixes.get(prefix) or self.__class__(self.t, self.r, self.b, self.l, self.color, prefix=prefix)
+                shadow = self.prefixes.get(prefix) or self.__class__(self.x, self.y, self.blur, self.color, prefix=prefix)
                 shadow.build(name, builder, prefix)
             sprefix = ''
         else:
